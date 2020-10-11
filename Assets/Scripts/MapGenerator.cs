5e2268a10ed895c607850abc46dffae6a29206f6
Ciremun <Ciremun@yandex.ru>
2020-10-11 16:04:41 +0300

    fixes

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using PathFind;
 
 
-public class RoomGenerator : MonoBehaviour
+public class MapGenerator : MonoBehaviour
 {
 
     public GameObject Wall;
@@ -16,23 +16,43 @@ public class RoomGenerator : MonoBehaviour
     private System.Random rnd = new System.Random();
     private Dictionary<string, List<Point>> levelDoors = new Dictionary<string, List<Point>>();
     private bool[,] levelGrid = new bool[100, 100];
-    private Point pathFindStart = new Point(25, 78);
+    private Point pathFindStart = new Point(50, 50);
 
     void Start()
     {
         for (int x = 0; x < levelGrid.GetLength(0); x++)
             for (int y = 0; y < levelGrid.GetLength(1); y++)
                 levelGrid[x, y] = true;
-        GenerateRoom(10, 80, rnd.Next(2, 15), rnd.Next(2, 15), 1);
-        GenerateRoom(30, 78, rnd.Next(2, 15), rnd.Next(2, 15), 1);
-        GenerateRoom(15, 56, rnd.Next(2, 15), rnd.Next(2, 15), 1);
-        GenerateRoom(44, 78, rnd.Next(2, 15), rnd.Next(2, 15), 1);
+        for (int i = 0; i < rnd.Next(20, 30); i++) // @@@ bug: spawn inside room blocks pathgen
+        {
+            int startX = rnd.Next(3, 75);
+            int startY = rnd.Next(3, 75);
+            int sizeX  = rnd.Next(3, 20);
+            int sizeY  = rnd.Next(3, 20);
+            while (!CheckGrid(startX, startY, sizeX, sizeY))
+            {
+                if (sizeX < 3 || sizeY < 3)
+                    break;
+                if (sizeX > sizeY)
+                    sizeX--;
+                else if (sizeY > sizeX)
+                    sizeY--;
+                else
+                {
+                    sizeX--;
+                    sizeY--;
+                }
+            }
+            if (sizeX < 3 || sizeY < 3)
+                continue;
+            GenerateRoom(startX, startY, sizeX, sizeY, rnd.Next(1, 2));
+        }
         List<Point> pathFloor = new List<Point>();
         PathFind.Grid grid = new PathFind.Grid(levelGrid);
         Instantiate(Floor, new Vector3(pathFindStart.x, pathFindStart.y, 0), Quaternion.identity);
-        levelGrid[pathFindStart.x, pathFindStart.y] = false;
         pathFloor.Add(new Point(pathFindStart.x, pathFindStart.y));
         foreach (var pair in levelDoors)
+        {
             foreach (var door in pair.Value)
             {
                 int doorX = door.x;
@@ -51,10 +71,14 @@ public class RoomGenerator : MonoBehaviour
                 foreach (var point in path)
                 {
                     Instantiate(Floor, new Vector3(point.x, point.y, 0), Quaternion.identity);
-                    levelGrid[point.x, point.y] = false;
                     pathFloor.Add(new Point(point.x, point.y));
                 }
             }
+        }
+        foreach (var point in pathFloor)
+        {
+            levelGrid[point.x, point.y] = false;
+        }
         foreach (var point in pathFloor)
         {
             List<Point> sides = new List<Point>{
@@ -64,11 +88,13 @@ public class RoomGenerator : MonoBehaviour
                 new Point(point.x, point.y - 1),
             };
             foreach (var pos in sides)
+            {
                 if (levelGrid[pos.x, pos.y])
                 {
                     Instantiate(Wall, new Vector3(pos.x, pos.y, 0), Quaternion.identity);
                     levelGrid[pos.x, pos.y] = false;
                 }
+            }
 
         }
         for (int x = 0; x < levelGrid.GetLength(0); x++)
@@ -80,6 +106,8 @@ public class RoomGenerator : MonoBehaviour
 
     private void GenerateRoom(int startX, int startY, int sizeX, int sizeY, int doorCount=0, string doorSide=null)
     {
+        if (sizeX == 0 || sizeY == 0)
+            return;
         int topX = sizeX + startX;
         int topY = sizeY + startY;
         Point[] roomCorners = {
@@ -124,9 +152,9 @@ public class RoomGenerator : MonoBehaviour
                     Walls["top"].Add(Instantiate(Wall, new Vector3(startX, nextY + 1, 0), Quaternion.identity));
                     levelGrid[startX, nextY + 1] = false;
                 } 
-                nextY += 1;
+                nextY++;
             }
-            startX += 1;
+            startX++;
         }
         var badDoorPositions = new Dictionary<string, List<(float, float)>>
         {
@@ -137,12 +165,15 @@ public class RoomGenerator : MonoBehaviour
         };
         for (int i = 0; i < doorCount; i++)
             CreateRoomDoor(ref Walls, ref badDoorPositions, doorSide);
+        Debug.Log("Room");
     }
 
     private void CreateRoomDoor(ref Dictionary<string, List<GameObject>> Walls, ref Dictionary<string, List<(float, float)>> badDoorPositions, string doorSide=null)
     {
         if (doorSide == null)
+        {
             doorSide = Walls.ElementAt(rnd.Next(0, Walls.Count)).Key;
+        }
         int RoomWallIndex = rnd.Next(Walls[doorSide].Count);
         GameObject RoomWall = Walls[doorSide][RoomWallIndex];
         int doorX = (int)RoomWall.transform.position.x;
@@ -158,7 +189,9 @@ public class RoomGenerator : MonoBehaviour
             badDoorPositions[doorSide].Add((doorX, doorY + 1));
         }
         if (badDoorPositions[doorSide].Any(i => i == (doorX, doorY)))
+        {
             return;
+        }
         Walls[doorSide].RemoveAt(RoomWallIndex);
         if (!levelDoors.ContainsKey(doorSide))
             levelDoors.Add(doorSide, new List<Point>());
@@ -166,5 +199,32 @@ public class RoomGenerator : MonoBehaviour
         Instantiate(Door, new Vector3(doorX, doorY, 0), Quaternion.identity);
         levelGrid[doorX, doorY] = false;
         Destroy(RoomWall);
+    }
+
+    private bool CheckGrid(int startX, int startY, int sizeX, int sizeY)
+    {
+        for (int x = 0; x < sizeX; x++)
+        {
+            int nextY = startY;
+            for (int y = 0; y < sizeY; y++)
+            {
+                List<Point> sides = new List<Point>{
+                    new Point(startX + 2, nextY),
+                    new Point(startX - 2, nextY),
+                    new Point(startX, nextY + 2),
+                    new Point(startX, nextY - 2),
+                };
+                foreach (var pos in sides)
+                {
+                    if (!levelGrid[pos.x, pos.y])
+                    {
+                        return false;
+                    }
+                }
+                nextY++;
+            }
+            startX++;
+        }
+        return true;
     }
 }
